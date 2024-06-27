33f092662f6db56780c5d69ffea03e71af153e53
abhitruechamp <abhiaruvs3@gmail.com>
2024-06-27 21:16:43 +0530

    Added logic to Xs and Os

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ let GameBoard = (function (){
     setSquare = (symbol, xPos, yPos) => {
         if (_ticTacToeArray[xPos][yPos] == null){
             _ticTacToeArray[xPos][yPos] = symbol;
+            return true;
+        }
+        else{
+            return false;
         }
     }
     return {displayBoard, returnGameBoard, setSquare};
@@ -22,6 +26,26 @@ let GameBoard = (function (){
 // let DisplayController = (function(){
 //     let gameBoard = GameBoard.returnGameBoard();
 // })
-GameBoard.displayBoard();
-GameBoard.setSquare("x", 1, 1);
-GameBoard.displayBoard();
+
+function playerFactory(symbol){
+    playTurn = (x, y) => {
+        if(GameBoard.setSquare(symbol, x,y)){
+            console.log("Square set");
+        }
+        else{
+            console.log("Square already taken");
+        }
+    }
+    return {playTurn}
+}
+function play(playerObject, xPos, yPos){
+    playerObject.playTurn(xPos, yPos);
+    GameBoard.displayBoard();
+}
+let game = function (){
+    const playerX = playerFactory('x');
+    const playerO = playerFactory('o');
+    play(playerX, 1, 1);
+    play(playerO, 0, 1);
+}
+game();
---
+++ test.md
@@ -0,0 +1,21 @@
+hjkl
+esc
+v -> Visual mode
+V -> Visual mode by line
+w/b -> Word
+d -> delete
+d3k -> Delete 3 lines above
+
+i -> insert mode one character next
+a -> insert mode one character above
+
+p -> Paste last thing
+y -> copy
+u -> undo
+ctrl + r -> Redo
+
+Extra:
+x -> delete letter
+$ -> Move last line
+^ -> Move start of line
+r -> Replace word
